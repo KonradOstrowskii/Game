41e6e62e6a0030c015496495f25aec1927029e80
konrad <ostrowskikonrad71@gmail.com>
2023-08-21 18:40:23 +0200

    adding race and their features

--- player.py
+++ player.py
@@ -7,13 +7,19 @@ class Player(object):
             name (str): Name of the player
             lives (int):  Lives of the player
             level (int): Level of the player
+            damage (int): Damage of the player
+            hit_points (int): Hit points of the player
             score (float): Score of the player
+            race (Race, object): Player's chosen race instance.
         """
-        self.name = name
+        self.player = name
         self._lives = 1
         self._level = 1
+        self._damage = 5
         self._hit_points = self._level * 10
         self._score = 0
+        self.race = None
+        self.race_name = None
 
     def _get_lives(self):
         return self._lives
@@ -30,6 +36,21 @@ class Player(object):
 
     def _get_level(self):
         return self._level
+    
+    def take_damage(self,damage):
+        remaining_points = self._hit_points - damage
+        if remaining_points >= 0:
+            self._hit_points = remaining_points
+            print("I took {} points damage and have {} left.".format(damage, self._hit_points))
+        else:
+            self._lives -=1
+            
+            if self._lives > 0:
+                print("{0._name} lost a life".format(self))
+                self._hit_points == self._hit_points 
+            else:
+                print("{0._name} is dead".format(self))
+                self.alive = False
 
 
     lives = property(_get_lives, _set_lives,_get_score)
@@ -41,7 +62,20 @@ class Player(object):
     @score.setter
     def score(self, score):
         self._score = score
+        
+    def apply_race_bonuses(self):
+        if self.race:
+            self.race_name = self.race.name
+            for attribute in dir(self.race):
+                if not attribute.startswith('__') and not callable(getattr(self.race, attribute)):
+                    setattr(self, attribute, getattr(self.race, attribute))
+                    if attribute == 'extra_hit_points':
+                        self._hit_points += getattr(self.race, attribute)  # Add extra hit points
+                    if attribute == 'bonus_dmg':
+                        self._damage += getattr(self.race, attribute)
+
 
     def __str__(self):
-        return "Name: {0.name}, Lives: {0.lives}, Level: {0.level}, Score {0.score}".format(self)
+        race_name = self.race.name if self.race else "No race"
+        return "Name: {0.player}, Lives: {0._lives},Damage: {0._damage} Level: {0._level}, Score {0._score},Hit Points {0._hit_points}, Race: {1}".format(self,race_name)
 
--- race.py
+++ race.py
@@ -1,18 +1,37 @@
+import random
+
 class Elf(object):
     """
     
      Dwarf class that player can choose early in the game
      
     """
-    def __init__(self, bonus_dmg = 6, extra_hit_points = 5):
+    def __init__(self,name ="Elf", bonus_dmg = 6, extra_hit_points = 5):
         """
 
         Args:
+            name (str): Elf
             bonus_dmg (int): Bonus damage . Defaults to 6.
             extra_hit_points (int): extra hit points . Defaults to 5.
         """
+        self.name = name
         self.bonus_dmg = bonus_dmg
         self.extra_hit_points = extra_hit_points
+        
+    def __str__(self):
+        description = "Elf : Graceful and intelligent beings, Elves are known for their agility and affinity for magic."
+        return description
+    
+    def dodges(self):
+        if random.randint(1,3) == 3:
+            print("**** {0.name} dodges *****".format(self))
+            return True
+        else:
+            return False
+    
+    def take_damage(self, damage):
+        if not self.dodges():
+            super().take_damage(damage)
         
 class Dwarf(object):
     """
@@ -20,12 +39,18 @@ class Dwarf(object):
      Dwarf class that player can choose early in the game
      
     """
-    def __init__(self, bonus_dmg = 3, extra_hit_points = 10):
+    def __init__(self,name = "Dwarf", bonus_dmg = 3, extra_hit_points = 10):
         """
 
         Args:
+            name (str): Dwarf
             bonus_dmg (int): Bonus damage . Defaults to 3.
             extra_hit_points (int): extra hit points . Defaults to 10.
         """
+        self.name = name
         self.bonus_dmg = bonus_dmg
         self.extra_hit_points = extra_hit_points
+        
+    def __str__(self):   
+        description = "Dwarf : Sturdy and resilient, Dwarves are skilled miners and craftsmen, with a natural resistance to damage."
+        return description
