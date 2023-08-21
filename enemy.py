94ab93fbf0bcd0d6cf288a4e194ca15df2eebfcf
konrad <ostrowskikonrad71@gmail.com>
2023-08-21 11:45:25 +0200

    add vampire monster

--- enemy.py
+++ enemy.py
@@ -9,26 +9,26 @@ class Monster:
             hit_points (int, optional): Hit points of the monster. Defaults to 0.
             lives (int, optional): Lives of the monster. Defaults to 1.
         """
-        self.name = name
-        self.hit_points = hit_points
-        self.lives = lives
+        self._name = name
+        self._hit_points = hit_points
+        self._lives = lives
         self.alive = True
         
     def take_damage(self,damage):
-        remaining_points = self.hit_points - damage
+        remaining_points = self._hit_points - damage
         if remaining_points >= 0:
-            self.hit_points = remaining_points
-            print("I took {} points damage and have {} left.".format(damage, self.hit_points))
+            self._hit_points = remaining_points
+            print("I took {} points damage and have {} left.".format(damage, self._hit_points))
         else:
-            self.lives -=1
-            if self.lives > 0:
-                print("{0.name} lost a life".format(self))
+            self._lives -=1
+            if self._lives > 0:
+                print("{0._name} lost a life".format(self))
             else:
-                print("{0.name} lis dead".format(self))
+                print("{0._name} is dead".format(self))
                 self.alive = False
             
     def __str__(self) -> str:
-        return "Name : {0.name}, Lives: {0.lives}, Hit points: {0.hit_points}".format(self)
+        return "Name : {0._name}, Lives: {0._lives}, Hit points: {0._hit_points}".format(self)
     
     
 class Troll(Monster):
@@ -41,9 +41,13 @@ class Vampire(Monster):
     def __init__(self, name="Enemy", hit_points=12, lives=3):
         super().__init__(name, hit_points, lives)
         
-    def doges(self):
+    def dodges(self):
         if random.randint(1,3) == 3:
-            print("****{0.name} dodges *****".format(self))
+            print("****{0._name} dodges *****".format(self))
             return True
         else:
             return False
+    
+    def take_damage(self, damage):
+        if not self.dodges():
+            super().take_damage(damage)
