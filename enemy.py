63c6c108ad17075745b52dc08eed84ca838f2667
konrad <ostrowskikonrad71@gmail.com>
2023-08-21 11:30:23 +0200

    rename Enemy to Monster

--- enemy.py
+++ enemy.py
@@ -1,9 +1,18 @@
-class Enemy:
-    
+import random
+class Monster:
+
     def __init__(self,name = "Enemy", hit_points = 0, lives= 1):
+        """_summary_
+
+        Args:
+            name (str, optional): Name of the monster. Defaults to "Enemy".
+            hit_points (int, optional): Hit points of the monster. Defaults to 0.
+            lives (int, optional): Lives of the monster. Defaults to 1.
+        """
         self.name = name
         self.hit_points = hit_points
         self.lives = lives
+        self.alive = True
         
     def take_damage(self,damage):
         remaining_points = self.hit_points - damage
@@ -12,12 +21,29 @@ class Enemy:
             print("I took {} points damage and have {} left.".format(damage, self.hit_points))
         else:
             self.lives -=1
+            if self.lives > 0:
+                print("{0.name} lost a life".format(self))
+            else:
+                print("{0.name} lis dead".format(self))
+                self.alive = False
             
     def __str__(self) -> str:
         return "Name : {0.name}, Lives: {0.lives}, Hit points: {0.hit_points}".format(self)
     
     
-class Troll(Enemy):
+class Troll(Monster):
     
     def __init__(self, name):
         super().__init__(name=name, hit_points=1, lives=1)
+        
+class Vampire(Monster):
+    
+    def __init__(self, name="Enemy", hit_points=12, lives=3):
+        super().__init__(name, hit_points, lives)
+        
+    def doges(self):
+        if random.randint(1,3) == 3:
+            print("****{0.name} dodges *****".format(self))
+            return True
+        else:
+            return False
