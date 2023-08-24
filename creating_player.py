0c3a9ae15e7dc0a625f0adc6e89260882e3435c5
konrad <ostrowskikonrad71@gmail.com>
2023-08-24 10:58:02 +0200

    adapting create_player

--- creating_player.py
+++ creating_player.py
@@ -15,27 +15,49 @@ def print_race_descriptions():
     print("3. Orc")
     print(Orc.__str__(Orc()))
 
+# Inside creating_player.py
+
 def create_player():
     player_name = input("Please choose your player name: ")
 
     print_race_descriptions()
 
     while True:
+        player = Player(player_name)
         race_choice = input("Choose a race (1 for Elf, 2 for Dwarf, 3 for Orc): ")
         if race_choice == "1":
-            Player.race = Elf()
-            Player.apply_race_bonuses(Elf)
+            player.race = Elf()
+            player.apply_race_bonuses()
+            weak_sword = Weapon("Very Not Sharp Sword", "A dull sword", 2)  # Bonus of 2 damage for Elf
+            weak_helmet = Helmet("Basic Cap", "A simple cap", 1)  # Bonus of 1 hit point for Elf
+            equipment = Equipment()
+            equipment.equip(weak_sword)
+            equipment.equip(weak_helmet)
+            player.equip_items(equipment)
             break
         elif race_choice == "2":
-            Player.race = Dwarf()
-            Player.apply_race_bonuses(Dwarf)
+            player.race = Dwarf()
+            player.apply_race_bonuses()
+            weak_hammer = Weapon("Weak Hammer", "A lightweight hammer", 1)  # Bonus of 1 damage for Dwarf
+            weak_helmet = Helmet("Plain Helmet", "A simple helmet", 1)  # Bonus of 1 hit point for Dwarf
+            equipment = Equipment()
+            equipment.equip(weak_hammer)
+            equipment.equip(weak_helmet)
+            player.equip_items(equipment)
             break
         elif race_choice == "3":
-            Player.race = Orc()
-            Player.apply_race_bonuses(Orc)
+            player.race = Orc()
+            player.apply_race_bonuses()
+            weak_axe = Weapon("Blunt Axe", "A dull axe", 1)  # Bonus of 1 damage for Orc
+            weak_helmet = Helmet("Old Helmet", "A worn helmet", 1)  # Bonus of 1 hit point for Orc
+            equipment = Equipment()
+            equipment.equip(weak_axe)
+            equipment.equip(weak_helmet)
+            player.equip_items(equipment)
             break
         else:
-            print("Invalid choice. Please choose 1 for Elf, 2 for Dwarf or 3 for Orc")
+            print("Invalid choice. Please choose 1 for Elf, 2 for Dwarf, or 3 for Orc")
 
     print("Player created:")
-    print(Player)
+    print(player)
+    return player  # Return the created player object
--- main.py
+++ main.py
@@ -7,20 +7,12 @@ from enemy import Vampire
 
 
 # Main code
-player = Player("")
-print(player)
-
-equipment = Equipment()
-sword = Weapon("Sword", "A sharp sword", 5)
-helmet = Helmet("Helmet", "A sturdy helmet", 3)
-equipment.equip(sword)
-equipment.equip(helmet)
-player.equip_items(equipment)
 print("Welcome to the Player Creation Menu!")
-create_player()
+player=create_player()
 
-cave_troll = Vampire(name="Vampire", hit_points=20, lives=3, damage=8)
+vampire = Vampire(name="Vampire", hit_points=20, lives=3, damage=2)
 
 # Start the fight
-fight = Fight(player, cave_troll)
+print(vampire)
+fight = Fight(player, vampire)
 fight.start()
