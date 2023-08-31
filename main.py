38f583cae1571b3d3682210b15f3a837abf1395e
konrad <ostrowskikonrad71@gmail.com>
2023-08-31 14:34:37 +0200

    .

--- main.py
+++ main.py
@@ -1,28 +1,9 @@
-from characters.player import Player 
-# from characters.equipment import  Equipment, Weapon, Helmet
 from characters.creating_player import create_player
 from combat.fight import Fight
 from characters.enemy import Vampire
 from saving_data.save_player import save_player_to_json, load_player_from_json
-import json
 
 
-
-
-
-
-# Main code
-# print("Welcome to the Player Creation Menu!")
-# player=create_player()
-# print(player)
-# player_filename = player.name
-# print(player.skills)
-# vampire = Vampire(name="Vampire", hit_points=20, lives=3, damage=2,experience_reward = 500)
-
-# # Start the fight
-# print(vampire)
-# fight = Fight(player, vampire)
-# fight.start()
 
 
 
