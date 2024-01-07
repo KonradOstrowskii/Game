27a37d106f0f1d83b05844ed3c69d9f1c363e905
Konrad <ostrowskikonrad71@gmail.com>
2024-01-07 21:05:50 +0100

    saving_player

--- main_app/main_app.py
+++ main_app/main_app.py
@@ -1,5 +1,6 @@
 from Game.characters.creating_player import create_player, print_race_descriptions
 from Game.combat.fight import Fight
+from Game.saving_data.save_player import save_player_to_json
 from Game.characters.enemy import *
 
 
@@ -7,12 +8,14 @@ def main():
     # Step 2: Create instances of Player and Vampire
     player = create_player()
     vampire = Troll("Count Dracula", hit_points=30, lives=1, damage=8, experience_reward=300)
+    #
+    # # Step 3: Create an instance of the Fight class
+    # vampire_fight = Fight(player, vampire)
+    #
+    # # Step 4: Start the fight
+    # vampire_fight.start()
 
-    # Step 3: Create an instance of the Fight class
-    vampire_fight = Fight(player, vampire)
-
-    # Step 4: Start the fight
-    vampire_fight.start()
+    save_player_to_json(player, 'postac.json')
 
 if __name__ == "__main__":
     main()
