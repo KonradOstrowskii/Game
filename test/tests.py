58f96d877d001a4c7c7ec8b249b2ea4df9037adc
Konrad <ostrowskikonrad71@gmail.com>
2024-06-29 10:07:21 +0100

    adjust_path

--- test/tests.py
+++ test/tests.py
@@ -1,7 +1,11 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from characters.player import Player
 from combat.fight import Fight
 from characters.enemy import Vampire
-
 
 # Test Player class
 def test_player_creation():
@@ -16,14 +20,12 @@ def test_player_creation():
     assert player.skills == {}
     assert player.alive is True
 
-
 def test_player_take_damage():
     player = Player(name="TestPlayer")
     initial_hit_points = player._hit_points
     damage_taken = 3
     player.take_damage(damage_taken)
     assert player._hit_points == initial_hit_points - damage_taken
-
 
 def test_player_level_up():
     player = Player(name="TestPlayer")
@@ -32,20 +34,24 @@ def test_player_level_up():
     assert player._damage == 7  # Check if damage increased after leveling up
     assert player._hit_points == 20  # Check if hit points increased after leveling up
 
-
 # Test Fight class
 def test_fight_start():
     player = Player(name="TestPlayer")
-    Vamp = Vampire("Vamp", 10, 1, 1, 200,alive=True)
-    fight = Fight(player, Vampire)
+    vamp = Vampire("Vamp", 10, 1, 1, 200, alive=True)
+    fight = Fight(player, vamp)
     fight.start()
-
 
 def test_enemy_take_damage():
     vampire = Vampire("Vamp", 10, 1, 1, 200, alive=True)
     initial_hit_points = vampire.hit_points
     damage_taken = 5
     vampire.take_damage(damage_taken)
-    assert vampire.hit_points > initial_hit_points - damage_taken
+    assert vampire.hit_points == initial_hit_points - damage_taken
 
-
+if __name__ == "__main__":
+    test_player_creation()
+    test_player_take_damage()
+    test_player_level_up()
+    test_fight_start()
+    test_enemy_take_damage()
+    print("All tests passed.")
